7cf681ec96f52d61cf649492e339ba5c24325865
mbettcher <mtonon.pmg@gmail.com>
2024-02-16 11:13:45 -0300

    Cria Module e Component de Cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ClientesModule } from './clientes/clientes.module'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClientesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
