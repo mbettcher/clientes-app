6a8de6e4ec6e6a6802651c2117cc0b790dacc3e0
mbettcher <mtonon.pmg@gmail.com>
2024-02-17 10:20:35 -0300

    Implementa script jquery para toggle button do sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'clientes-app';
+
+  ngAfterViewInit(): void {
+    window.addEventListener('DOMContentLoaded', event => {
+
+      // Toggle the side navigation
+      const sidebarToggle = document.body.querySelector('#sidebarToggle');
+      if (sidebarToggle) {
+          // Uncomment Below to persist sidebar toggle between refreshes
+          // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+          //     document.body.classList.toggle('sb-sidenav-toggled');
+          // }
+          sidebarToggle.addEventListener('click', event => {
+              event.preventDefault();
+              document.body.classList.toggle('sb-sidenav-toggled');
+              localStorage.setItem('sb|sidebar-toggle', new Boolean(document.body.classList.contains('sb-sidenav-toggled')).toString());
+          });
+      }
+  
+  });
+  }
 }
