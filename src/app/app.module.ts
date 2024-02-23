f7c3a1a160593a2208c00d2059125b7a699ad8e5
mbettcher <mtonon.pmg@gmail.com>
2024-02-23 17:38:22 -0300

    Conclui o Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ClientesModule } from './clientes/clientes.module'
-import { TemplateModule } from './template/template.module'
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
