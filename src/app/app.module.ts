f6ca227cfa5f0bae6e7e6654a3585461ded2dbf1
mbettcher <mtonon.pmg@gmail.com>
2024-02-24 12:22:35 -0300

    Importando o HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 import { TemplateModule } from './template/template.module';
 import { HomeComponent } from './home/home.component'
@@ -15,6 +16,7 @@ import { ClientesService } from './clientes.service';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     TemplateModule,
     ClientesModule
