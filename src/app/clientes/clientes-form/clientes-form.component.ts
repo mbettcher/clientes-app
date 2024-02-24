b475cd19b9acb13c4be40299e942dec6ed516fdf
mbettcher <mtonon.pmg@gmail.com>
2024-02-24 11:04:00 -0300

    Cria classe Cliente e Campos formulário Cliente

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../cliente'
 
 @Component({
   selector: 'app-clientes-form',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesFormComponent implements OnInit {
 
+  cliente: Cliente;
+  
   constructor() { }
 
   ngOnInit(): void {
