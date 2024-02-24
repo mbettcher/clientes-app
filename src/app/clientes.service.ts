cc819ed06e7ef5e3c0cf7e1b3657e13babcd0a07
mbettcher <mtonon.pmg@gmail.com>
2024-02-24 12:48:22 -0300

    Implementando salvar(Cliente) e comunicando com a API Clientes

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Cliente } from './clientes/cliente';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,9 @@ export class ClientesService {
 
   constructor( private http: HttpClient ) { }
 
+  salvar(cliente: Cliente) : Observable<Cliente> {
+    return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
+  }
   getCliente() : Cliente {
     let cliente : Cliente = new Cliente();
     cliente.nome = 'Marcelo Tonon';
